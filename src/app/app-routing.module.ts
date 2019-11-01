e9e646952713670081de183a18bb0648b4181d57
Luiza <luizapereiratvd@gmail.com>
2019-11-01 16:08:51 -0300

    Criando as rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
+import { CharacterListComponent } from './components/character-list/character-list.component';
+import { PlanetsListComponent } from './components/planets-list/planets-list.component';
+import { VehiclesListComponent } from './components/vehicles-list/vehicles-list.component';
+import { SpeciesListComponent } from './components/species-list/species-list.component';
+import { SpaceshipsListComponent } from './components/spaceships-list/spaceships-list.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'Movies', component: MovieDetailComponent },
+  { path: 'Characters', component: CharacterListComponent },
+  { path: 'Planets', component: PlanetsListComponent },
+  { path: 'Vehicles', component: VehiclesListComponent },
+  { path: 'Species', component: SpeciesListComponent },
+  { path: 'Spaceships', component: SpaceshipsListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
+import { CharacterListComponent } from './components/character-list/character-list.component';
+import { PlanetsListComponent } from './components/planets-list/planets-list.component';
+import { VehiclesListComponent } from './components/vehicles-list/vehicles-list.component';
+import { SpeciesListComponent } from './components/species-list/species-list.component';
+import { SpaceshipsListComponent } from './components/spaceships-list/spaceships-list.component';
+import { HomeComponent } from './components/home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MovieDetailComponent,
+    CharacterListComponent,
+    PlanetsListComponent,
+    VehiclesListComponent,
+    SpeciesListComponent,
+    SpaceshipsListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
