e54d860dea9d8fad0d0c97a247d67a80319b229d
Luiza <luizapereiratvd@gmail.com>
2019-11-01 16:06:38 -0300

    Criando os componentes

---
+++ src/app/components/spaceships-list/spaceships-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-spaceships-list',
+  templateUrl: './spaceships-list.component.html',
+  styleUrls: ['./spaceships-list.component.scss']
+})
+export class SpaceshipsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
