c0b43dce5b41fa70b7706ac37c09307ab5319109
Luiza <luizapereiratvd@gmail.com>
2019-11-12 17:50:24 -0300

    Ajeitando as pastas do projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
+import { MovieDetailComponent } from './components/pages/movie-detail/movie-detail.component';
 import { CharacterListComponent } from './components/character-list/character-list.component';
 import { PlanetsListComponent } from './components/planets-list/planets-list.component';
 import { VehiclesListComponent } from './components/vehicles-list/vehicles-list.component';
 import { SpeciesListComponent } from './components/species-list/species-list.component';
 import { SpaceshipsListComponent } from './components/spaceships-list/spaceships-list.component';
-import { AboutComponent } from './components/about/about.component';
+import { AboutComponent } from './components/pages/about/about.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,20 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
+import { MovieDetailComponent } from './components/pages/movie-detail/movie-detail.component';
 import { CharacterListComponent } from './components/character-list/character-list.component';
 import { PlanetsListComponent } from './components/planets-list/planets-list.component';
 import { VehiclesListComponent } from './components/vehicles-list/vehicles-list.component';
 import { SpeciesListComponent } from './components/species-list/species-list.component';
 import { SpaceshipsListComponent } from './components/spaceships-list/spaceships-list.component';
 import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
+import { AboutComponent } from './components/pages/about/about.component';
 import { HttpClientModule } from '@angular/common/http';
 import { InformationBoxComponent } from './components/information-box/information-box.component';
 import { BackButtonComponent } from './components/back-button/back-button.component';
 import { LoadingBoxComponent } from './components/loading-box/loading-box.component';
 import { FilterPipe } from './pipe/filter.pipe';
-import { ErrorPageComponent } from './error-page/error-page.component';
+import { ErrorPageComponent } from './components/pages/error-page/error-page.component';
 
 
 @NgModule({
