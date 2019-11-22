c188050505339f7f4cee3c2f6b18785e343e7346
Luiza <luizapereiratvd@gmail.com>
2019-11-22 14:35:02 -0300

    Changing pages location

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { MovieDetailComponent } from './components/pages/movie-detail/movie-detail.component';
-import { HomeComponent } from './components/pages/home/home.component';
-import { AboutComponent } from './components/pages/about/about.component';
+import { MovieDetailComponent } from './pages/movie-detail/movie-detail.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 import { HttpClientModule } from '@angular/common/http';
 import { InformationBoxComponent } from './components/information-box/information-box.component';
 import { BackButtonComponent } from './components/back-button/back-button.component';
 import { LoadingBoxComponent } from './components/loading-box/loading-box.component';
 import { FilterPipe } from './pipe/filter.pipe';
-import { ErrorPageComponent } from './components/pages/error-page/error-page.component';
-import { BuildingPageComponent } from './components/pages/building-page/building-page.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { BuildingPageComponent } from './pages/building-page/building-page.component';
 
 
 @NgModule({
